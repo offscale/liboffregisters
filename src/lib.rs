6898a9e76363b70e61c122e0f269d566c50e021a
Samuel Marks <samuelmarks@gmail.com>
2019-02-11 17:22:42 +1100

    [download] Improved error handling & percolation, tests, and return types

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,43 @@
 #[macro_use]
 extern crate failure;
 
+#[macro_use]
+extern crate lazy_static;
+
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
 use mio::{Events, Poll};
-use mio_httpc::{CallBuilder, Httpc, HttpcCfg, SimpleCall};
+use mio_httpc::{CallBuilder, Headers, Httpc, HttpcCfg, SimpleCall};
 
 use url::Url;
 
 use failure::Error;
 
-fn do_call(htp: &mut Httpc, poll: &Poll, mut call: SimpleCall) -> Result<String, Error> {
-    let mut buf: Option<String>;
+#[derive(Clone)]
+pub struct StatusHeadersText<'a> {
+    status: u16,
+    headers: Headers<'a>,
+    text: String,
+}
+
+fn do_call<'a>(
+    htp: &mut Httpc,
+    poll: &Poll,
+    mut call: SimpleCall,
+) -> Result<StatusHeadersText<'a>, Error> {
+    let mut opt_resp: Option<String>;
     let to = ::std::time::Duration::from_millis(100);
     let mut events = Events::with_capacity(8);
+
+    let last_status: u16;
+    /* TODO:
+    let last_headers: &'a Headers ;
+    */
     'outer: loop {
-        poll.poll(&mut events, Some(to)).unwrap();
+        poll.poll(&mut events, Some(to))?;
         for cref in htp.timeout().into_iter() {
             if call.is_ref(cref) {
                 return Err(format_err!("Request timed out"));
@@ -30,14 +49,16 @@ fn do_call(htp: &mut Httpc, poll: &Poll, mut call: SimpleCall) -> Result<String,
         for ev in events.iter() {
             let cref = htp.event(&ev);
 
-            if call.is_call(&cref) && call.perform(htp, &poll).expect("Call failed") {
-                let (resp, body) = call.finish().expect("No response");
-                println!("done req = {}", resp.status);
-                for h in resp.headers() {
-                    println!("Header = {}", h);
-                }
+            if call.is_call(&cref) && call.perform(htp, &poll)? {
+                let (response, body) = match call.finish() {
+                    Some(rb) => Ok(rb),
+                    None => Err(format_err!("No response")),
+                }?;
                 match String::from_utf8(body.clone()) {
-                    Ok(s) => buf = Some(s),
+                    Ok(s) => {
+                        opt_resp = Some(s);
+                        last_status = response.status;
+                    }
                     Err(_) => {
                         return Err(format_err!("Non utf8 body sized: {}", body.len()));
                     }
@@ -46,13 +67,16 @@ fn do_call(htp: &mut Httpc, poll: &Poll, mut call: SimpleCall) -> Result<String,
             }
         }
     }
-    match buf {
-        Some(s) => Ok(s),
+
+    match opt_resp {
+        Some(text) => Ok(StatusHeadersText {
+            status: last_status,         // last_response.status.clone(),
+            headers: Headers::default(), // last_response.headers().clone(),
+            text,
+        }),
         None => Err(format_err!("Empty response")),
     }
 }
-
-//const URLS: &'static [&'static str] = &["http://detectportal.firefox.com/success.txt"];
 
 pub fn basename(path: &str) -> String {
     match path.rsplit(std::path::MAIN_SEPARATOR).next() {
@@ -61,8 +85,11 @@ pub fn basename(path: &str) -> String {
     }
 }
 
-pub fn download(dir: Option<&str>, urls: Vec<&str>) -> Result<HashMap<String, String>, Error> {
-    let mut url2response: HashMap<String, String> = HashMap::new();
+pub fn download(
+    dir: Option<&str>,
+    urls: Vec<Url>,
+) -> Result<HashMap<Url, StatusHeadersText>, Error> {
+    let mut url2response: HashMap<Url, StatusHeadersText> = HashMap::new();
 
     let poll = Poll::new()?;
 
@@ -73,37 +100,30 @@ pub fn download(dir: Option<&str>, urls: Vec<&str>) -> Result<HashMap<String, St
     let mut htp = Httpc::new(10, Some(cfg));
 
     for i in 0..urls.len() {
+        let url = &urls[i];
+        let url_s = url.clone().into_string();
+
         let call = CallBuilder::get()
-            .url(urls[i])? // .expect("Invalid url")
+            .url(&url_s)
+            .unwrap()
             .timeout_ms(10000)
             // .insecure_do_not_verify_domain()
             .simple_call(&mut htp, &poll)?; // .expect("Call start failed");
 
         let mut error: Option<Error> = None;
         let download_path: Option<String> = if dir.is_some() {
-            let uri_opt = match Url::parse(urls[i]) {
-                Ok(uri) => Some(uri),
-                Err(e) => {
-                    error = Some(format_err!("{}", e.to_string()));
-                    None
-                }
-            };
-            if uri_opt.is_none() {
+            let p = Path::new(&dir.unwrap())
+                .join(Path::new(&basename(&url_s)))
+                .to_string_lossy()
+                .into_owned();
+            if p.is_empty() {
+                error = Some(format_err!(
+                    "Conversion to filename failed for: {:#?}",
+                    urls[i]
+                ));
                 None
             } else {
-                let p = Path::new(&dir.unwrap())
-                    .join(Path::new(&basename(uri_opt.unwrap().path())))
-                    .to_string_lossy()
-                    .into_owned();
-                if p.is_empty() {
-                    error = Some(format_err!(
-                        "Conversion to filename failed for: {:#?}",
-                        urls[i]
-                    ));
-                    None
-                } else {
-                    Some(p)
-                }
+                Some(p)
             }
         } else {
             None
@@ -117,19 +137,27 @@ pub fn download(dir: Option<&str>, urls: Vec<&str>) -> Result<HashMap<String, St
             return Err(format_err!("No filename detectable from URL"));
         }
 
-        match do_call(&mut htp, &poll, call) {
-            Ok(response_string) => url2response.insert(
-                urls[i].into(),
-                String::from(if dir.is_some() {
+        let (k, v) = (match do_call(&mut htp, &poll, call) {
+            Ok(status_headers_text) => {
+                match if dir.is_some() && download_path.is_some() {
                     let dp = download_path.unwrap();
-                    write_file(Path::new(&dp), response_string)?;
-                    dp
+                    write_file(Path::new(&dp), status_headers_text.text)?;
+                    Ok(StatusHeadersText {
+                        headers: status_headers_text.headers,
+                        status: status_headers_text.status,
+                        text: dp,
+                    })
                 } else {
-                    response_string
-                }),
-            ),
-            Err(e) => return Err(format_err!("{}", e)),
-        };
+                    Ok(status_headers_text)
+                } {
+                    Ok(v) => Ok((urls[i].clone(), v)),
+                    Err(e) => Err(e),
+                }
+            }
+            Err(e) => return Err(e),
+        } as Result<(Url, StatusHeadersText), Error>)?;
+
+        url2response.insert(k, v);
 
         println!("Open connections = {}", htp.open_connections());
     }
@@ -167,20 +195,72 @@ pub fn env_or(key: &str, default: std::ffi::OsString) -> std::ffi::OsString {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::env::temp_dir;
 
-    const URLS: &'static [&'static str] = &["http://detectportal.firefox.com/success.txt"];
+    #[inline(always)]
+    fn urls2urls() -> Vec<Url> {
+        URLRESPONSES
+            .iter()
+            .map(|url_response| Url::parse(url_response.url).unwrap())
+            .collect()
+    }
+
+    const URLRESPONSES: &'static [&'static UrlResponse] = &[
+        &UrlResponse {
+            url: "http://detectportal.firefox.com/success.txt",
+            status: 200,
+            content: "success\n",
+            fname: "success.txt",
+        },
+        &UrlResponse {
+            url: "http://www.msftncsi.com/ncsi.txt",
+            status: 200,
+            content: "Microsoft NCSI",
+            fname: "ncsi.txt",
+        },
+        &UrlResponse {
+            url: "http://connectivitycheck.gstatic.com/generate_204",
+            status: 204,
+            content: "",
+            fname: "generate_204",
+        },
+        &UrlResponse {
+            url: "http://www.apple.com/library/test/success.html",
+            status: 200,
+            content: "<HTML><HEAD><TITLE>Success</TITLE></HEAD><BODY>Success</BODY></HTML>",
+            fname: "success.html",
+        },
+    ];
+
+    struct UrlResponse {
+        url: &'static str,
+        status: u16,
+        content: &'static str,
+        fname: &'static str,
+    }
+
+    lazy_static! {
+        static ref URLS: Vec<Url> = urls2urls();
+    }
 
     #[test]
     fn download_to_dir() {
-        let _td = temp_dir();
+        let _td = std::env::temp_dir();
         let _td_cow = _td.to_string_lossy();
         let tmp_dir = _td_cow.as_ref();
-        match download(Some(tmp_dir), URLS.to_vec()) {
+        let urls = urls2urls();
+        match download(Some(tmp_dir), urls) {
             Ok(url2response) => {
-                for (url, response) in &url2response {
-                    assert_eq!(url, URLS[0]);
-                    assert_eq!(Path::new(response), Path::new(tmp_dir).join("success.txt"))
+                for &expected_url_response in URLRESPONSES {
+                    let url: &Url = &Url::parse(expected_url_response.url).unwrap();
+                    assert_eq!(url2response.contains_key(url), true);
+                    let actual_response = url2response.get(url).unwrap();
+                    assert_eq!(
+                        Some(actual_response.text.as_str()),
+                        Path::new(tmp_dir)
+                            .join(expected_url_response.fname)
+                            .to_str()
+                    );
+                    assert_eq!(actual_response.status, expected_url_response.status)
                 }
             }
             Err(e) => panic!(e),
@@ -191,9 +271,12 @@ mod tests {
     fn download_to_mem() {
         match download(None, URLS.to_vec()) {
             Ok(url2response) => {
-                for (url, response) in &url2response {
-                    assert_eq!(url, URLS[0]);
-                    assert_eq!(response, "success\n")
+                for &expected_url_response in URLRESPONSES {
+                    let url: &Url = &Url::parse(expected_url_response.url).unwrap();
+                    assert_eq!(url2response.contains_key(url), true);
+                    let actual_response = url2response.get(url).unwrap();
+                    assert_eq!(actual_response.text.as_str(), expected_url_response.content);
+                    assert_eq!(actual_response.status, expected_url_response.status)
                 }
             }
             Err(e) => panic!(e),
